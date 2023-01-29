50e80a76c81ab8aa3e331edcc81ee3733143e5bc
Natã <brnatajose@gmail.com>
2023-01-28 20:51:34 -0400

    Melhorias no layout e inclusão do Vlibras

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Portfolio.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacidade()
         {
             return View();
         }
